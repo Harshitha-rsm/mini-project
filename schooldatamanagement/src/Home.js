3fcefd59085c8eb59301f3403fda11152fa6db33
pujikaupadhyay-rsm <Pujika.Upadhyay@rsmus.com>
2022-08-08 14:03:32 +0530

    Working 'Sign In/ Sign Up' Pages. To Do: Apply CSS and Material UI

--- schooldatamanagement/src/Home.js
+++ schooldatamanagement/src/Home.js
@@ -1,30 +1,31 @@
 import {TextField} from '@mui/material';
 import {Grid,Card,CardContent,Typography,Button} from '@mui/material'
-import { Routes,Route,BrowserRouter } from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import { Link } from "react-router-dom"
-import {SignInPage} from './SignInPage'
-import {SignUpPage} from './SignUpPage'
-export default function Home(){
-  // <Routes>
-  //   <Route path='./SignIn' element={<SignInPage/>}/>
-  //   <Route path='./SignUp' element={<SignUpPage/>}/>
-  // </Routes>
-  return(<Grid style={{padding:"80px 5px 0 5px"}} container spacing={2} columns={16}>
-    <Grid item xs={8}>
-        <Card style={{maxWidth:512, margin:"0 auto",textAlign:"center",fontSize:'24px',color:'blueviolet'}}>SIGN IN</Card>
-        </Grid>
-    <Grid item xs={8}>
-      <Card style={{maxWidth:512,margin:"0 auto",textAlign:"center",fontSize:'24px',color:'blueviolet'}}>SIGN UP</Card>    
-    </Grid>
-  </Grid>)
-}
+import  SignInPage  from './SignInPage'
+import SignUpPage from './SignUpPage'
+// export default function Home(){
 
+//   return(<Grid style={{padding:"80px 5px 0 5px"}} container spacing={2} columns={16}>
+ 
+//    <Grid item xs={8}>
+//         <Card style={{maxWidth:512, margin:"0 auto",textAlign:"center",fontSize:'24px',color:'blueviolet'}}>SIGN IN</Card>
+//         </Grid>
+//     <Grid item xs={8}>
+//       <Card style={{maxWidth:512,margin:"0 auto",textAlign:"center",fontSize:'24px',color:'blueviolet'}}>SIGN UP</Card>    
+//     </Grid>
+//   </Grid>)
+// }
 
 export const Navbar =() =>{
   return(
   <nav>
-  <Link to='/SignIn'>Sign In</Link>
-  <Link to='/SignUp'>Sign Up</Link>
+  <Link to='/SignIn'>Sign In</Link> &ensp;
+  <Link to='/SignUp'>Sign Up</Link> 
+  <Routes>
+    <Route path='/SignIn' element={<SignInPage/>}/> {/*need not mention './' here.*/}
+    <Route path='/SignUp' element={<SignUpPage/>}/>
+  </Routes>
   </nav>
   )
   }
